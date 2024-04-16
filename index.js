a16e3bfb799890af8018156f2fc7eb3157a6c4a5
Aman <amanjaiswar54@gmail.com>
2024-04-16 16:00:45 +0530

    done with curd

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const fs = require('fs')
 const path = require('path')
-const dirPath = path.join(__dirname, 'files')
-// for(i=0; i<5; i++){
-//     fs.writeFileSync(`${dirPath}/hello${i}.text`,"this is s simple text file")
-// }
-fs.readdir(dirPath,(err,files)=>{
-    // console.log(files)
-    files.forEach((item)=>{
-        console.log(`file name is ${item}`)
-    })
-})
+const dirPath = path.join(__dirname, 'curd')
+const filepath = `${dirPath}/file.txt`
+
+// fs.writeFileSync(filepath,'this is simple txt file')
+// fs.readFile(filepath,'utf8',(err, item)=>{
+//     console.log(item)
+// })
+// fs.appendFile(filepath,'and more text in this file',(err)=>{
+//     if(!err) console.log('file is updated')
+// })
+// fs.rename(filepath,`${dirPath}/aman.txt`,(err)=>{
+//     if(!err) console.log('file name is updated')
+// })
+fs.unlinkSync(`${dirPath}/aman.txt`)
